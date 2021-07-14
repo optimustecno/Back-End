825d4ce4146e1e68beb55c6f080ab18d120c411c
mcomaycon <82045987+mcomaycon@users.noreply.github.com>
2021-07-14 18:20:51 -0300

    botão

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
+import React from "react";
+
 function Home() {
     return (
-        <div>HOME</div>
+        <div>
+            <p>HOME</p>
+            <button>
+                Teste
+            </button>
+        </div>
     )
 }
 

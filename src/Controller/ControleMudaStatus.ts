e369a922186ad3e456ca8cbc0d052ab8da448c2e
mcomaycon <82045987+mcomaycon@users.noreply.github.com>
2021-10-25 15:36:00 -0300

    AJUSTE NA ATUALIZAÇÃO DE STATUS UAI RANGO

--- src/Controller/ControleMudaStatus.ts
+++ src/Controller/ControleMudaStatus.ts
@@ -18,27 +18,33 @@ class ControleMudancaStatus {
         peds.forEach(async ped => {
 
             if (ped.app === "UAI RANGO") {
-                var motivo = JSON.stringify({
-                    "motivo": "Pedido cancelado. Caso necessário entre em contato com o estabelecimento."
-                })
-                var requestOptionsUai = {
-                    method: 'POST',
-                    headers: {
-                        "Authorization": `Bearer ${process.env.TOKEN_UAI_RANGO}`,
-                        "Content-Type": "application/json"
-                    },
-                    body: motivo
-                };
                 if (ped.novo_status === "7") {
+                    var motivo = JSON.stringify({
+                        "motivo": "Pedido cancelado. Caso necessário entre em contato com o estabelecimento."
+                    })
+                    var requestOptionsUai = {
+                        method: 'POST',
+                        headers: {
+                            "Authorization": `Bearer ${process.env.TOKEN_UAI_RANGO}`,
+                            "Content-Type": "application/json"
+                        },
+                        body: motivo
+                    };
                     var UaiResponse = await fetch(`https://www.uairango.com/api2/auth/pedido/cancela/${ped.opt_pedido_app}`, requestOptionsUai);
+                    if (UaiResponse.status === 200) {
+                        var AtualizaStatus = await ExecuteSQL(
+                            `UPDATE opt_ped_app SET status = '${ped.novo_status}'
+                            WHERE opt_pedido_app = ?`, ped.opt_pedido_app
+                        )
+                    }
                 }
-                //var AcconResponseJson = await AcconResponse.json();
-                if (UaiResponse.status === 200) {
+                else {
                     var AtualizaStatus = await ExecuteSQL(
                         `UPDATE opt_ped_app SET status = '${ped.novo_status}'
                         WHERE opt_pedido_app = ?`, ped.opt_pedido_app
                     )
                 }
+
             }
             else if (ped.app === "ACCON") {
                 var requestOptions = {

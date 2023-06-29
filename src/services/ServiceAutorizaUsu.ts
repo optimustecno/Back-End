5d1b3d8820ab3e0d65c0ae05983376cc876e8ee6
mcomaycon <82045987+mcomaycon@users.noreply.github.com>
2023-06-29 17:28:09 -0300

    Ajuste no objeto de usuarios

--- src/services/ServiceAutorizaUsu.ts
+++ src/services/ServiceAutorizaUsu.ts
@@ -9,34 +9,36 @@ interface iAutenticaUsu {
 }
 
 class AutorizaUsu {
-
     async execute({ email, senha }: iAutenticaUsu) {
         const usuario = getCustomRepository(UsuarioRep);
         //usuario ok?
         const user = await usuario.findOne({
-            opt_email: email
-        })
+            opt_email: email,
+        });
         if (!user) {
-            throw new Error("Email ou Senha Incorretos")
+            throw new Error("Email ou Senha Incorretos");
         }
         //senha ok?
         const bEncontrouSenha = await compare(senha, user.opt_senha);
         if (!bEncontrouSenha) {
-            throw new Error("Email ou Senha Incorretos")
+            throw new Error("Email ou Senha Incorretos");
         }
         const cCodigo = user.opt_codigo_usu.toString();
         //Gera Token
-        const token = sign({
-            codigo: user.opt_codigo_usu,
-            nome: user.opt_usuario
-        }, process.env.SECRET,
+        const token = sign(
+            {
+                codigo: user.opt_codigo_usu,
+                nome: user.opt_usuario,
+            },
+            process.env.SECRET,
             {
                 subject: cCodigo,
-                expiresIn: "1d"
-            });
+                expiresIn: "1d",
+            }
+        );
 
-        return token
+        return { codigo: cCodigo, nome: user.opt_usuario, token };
     }
 }
 
-export { AutorizaUsu }
+export { AutorizaUsu };

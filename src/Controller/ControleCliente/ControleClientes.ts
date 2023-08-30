084229f1811ea64aacc17aa3b84bbae8c9a4dc0e
mcomaycon <82045987+mcomaycon@users.noreply.github.com>
2023-08-30 15:01:36 -0300

    Ajuste na Busca de Clientes

--- src/Controller/ControleCliente/ControleClientes.ts
+++ src/Controller/ControleCliente/ControleClientes.ts
@@ -5,12 +5,14 @@ class ControleListaClientes {
     async handle(request: Request, response: Response) {
         var opt_bloqueado = request.query.opt_bloqueado;
         var opt_nome_sistema = request.query.opt_nome_sistema;
+        var opt_nome_cliente = request.query.opt_nome_cliente;
 
         const consultacli = new ServiceListaClientes();
 
         const clientes = await consultacli.execute({
             opt_bloqueado,
             opt_nome_sistema,
+            opt_nome_cliente,
         });
 
         return response.json(clientes);
--- src/services/ServiceCliente/ServiceRetClientes.ts
+++ src/services/ServiceCliente/ServiceRetClientes.ts
@@ -1,18 +1,30 @@
-import { getCustomRepository } from "typeorm";
+import { getCustomRepository, Like } from "typeorm";
 import { ClientesRep } from "../../repositories/ClientesRep";
 
 interface iFiltro {
     opt_bloqueado?: any;
     opt_nome_sistema?: any;
+    opt_nome_cliente?: any;
 }
 
 class ServiceListaClientes {
-    async execute({ opt_bloqueado, opt_nome_sistema }: iFiltro) {
+    async execute({ opt_bloqueado, opt_nome_sistema, opt_nome_cliente }: iFiltro) {
         var ClientesCad;
 
         const clientesRep = getCustomRepository(ClientesRep);
 
-        if (opt_bloqueado && opt_nome_sistema) {
+        if (opt_bloqueado && opt_nome_sistema && opt_nome_cliente) {
+            ClientesCad = await clientesRep.find({
+                where: {
+                    opt_bloqueado,
+                    opt_nome_sistema,
+                    opt_nome_cliente: Like(`%${opt_nome_cliente}%`),
+                },
+                order: {
+                    opt_nome_cliente: "ASC",
+                },
+            });
+        } else if (opt_bloqueado && opt_nome_sistema) {
             ClientesCad = await clientesRep.find({
                 where: {
                     opt_bloqueado,
@@ -22,8 +34,18 @@ class ServiceListaClientes {
                     opt_nome_cliente: "ASC",
                 },
             });
-        } else if (!opt_bloqueado && !opt_nome_sistema) {
+        } else if (!opt_bloqueado && !opt_nome_sistema && !opt_nome_cliente) {
             ClientesCad = await clientesRep.find({
+                order: {
+                    opt_nome_cliente: "ASC",
+                },
+            });
+        } else if (!opt_bloqueado && !opt_nome_sistema) {
+            var NomeCli = opt_nome_cliente;
+            ClientesCad = await clientesRep.find({
+                where: {
+                    opt_nome_cliente: Like(`%${opt_nome_cliente}%`),
+                },
                 order: {
                     opt_nome_cliente: "ASC",
                 },
